d26479b11e91a03348d7ac198a45ffd1a48cb575
Daniel LeJeune <daniel.lejeune327@gmail.com>
2015-09-12 15:32:55 -0400

    image uploads work

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, \
+	url_for
+from werkzeug import secure_filename
 import os
 import base64
 import json
@@ -10,9 +12,17 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'bmp'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def hello():
 	return render_template('index.html')
+
+@app.route('/2')
+def hello2():
+	return render_template('index2.html')
 
 @app.route('/css/<path:path>')
 def send_css(path):
@@ -34,6 +44,20 @@ def getimage():
 	n = 256
 	data = {'base64': image64.decode('utf-8')}
 	return json.dumps(data) #'{"base64": "%s"}' % image64
+
+@app.route('/putimage', methods=['POST'])
+def putimage():
+	file = request.files['file']
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return redirect(url_for('uploaded_file', filename=filename))
+
+@app.route('/uploaded', methods=['GET'])
+def uploaded_file():
+	val = request.args.get('filename')
+	return val
+	
 
 if __name__ == '__main__':
 	app.run(
